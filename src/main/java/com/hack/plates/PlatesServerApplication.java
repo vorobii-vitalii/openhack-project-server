a61c70558c2d80782b3eb15e5fe8931059284c1b
vitalii <vitalii.vorobij@gmail.com>
2020-05-24 15:10:24 +0300

    Added entry-point java class

---
+++ src/main/java/com/hack/plates/PlatesServerApplication.java
@@ -0,0 +1,13 @@
+package com.hack.plates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatesServerApplication.class, args);
+	}
+
+}
